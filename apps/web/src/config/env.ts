6908f51db365f9b112d14f484e3aa0af4cdfee3a
hrithikroboto <hrithik@robotostudio.com>
2024-09-05 00:54:21 +0530

    refactor: Update SANITY_API_READ_TOKEN to SANITY_API_TOKEN

--- apps/web/src/config/env.ts
+++ apps/web/src/config/env.ts
@@ -50,7 +50,7 @@
 
 const getBaseUrl = (): string => {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production')
-    return process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL as string;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview')
     return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
   return 'http://localhost:3000';
